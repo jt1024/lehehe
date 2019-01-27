bc2cc6c08777018842be04a6e4f8aa24cbf160f5
麦典威 <cctvjiatao@163.com>
2019-01-27 20:11:04 +0800

    modify password

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,10 @@ app_name = 'account'  # 一定要写这一行，否则html中会报错 'account'
 
 urlpatterns = [
     # path(r'login/', views.user_login, name='user_login'),
-    path(r'login/', auth_views.LoginView.as_view(template_name="account/login.html"), name='user_login'),  # 使用Django内置的登录方法
-    path(r'logout/', auth_views.LogoutView.as_view(template_name="account/logout.html"), name='user_logout'),  # 使用Django内置的登出方法
-    path(r'register/', views.user_register, name='user_register'),
+    path('login/', auth_views.LoginView.as_view(template_name="account/login.html"), name='user_login'),  # 使用Django内置的登录方法
+    path('logout/', auth_views.LogoutView.as_view(template_name="account/logout.html"), name='user_logout'),  # 使用Django内置的登出方法
+    path('register/', views.user_register, name='user_register'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name="account/password_change_form.html", success_url="/account/password-change-done/"), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="account/password_change_done.html"), name='password_change_done'),
+
 ]
