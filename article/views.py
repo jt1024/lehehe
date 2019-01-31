6072e9a161dc7307293967c9b126ea05714de996
jt1024 <cctvjiatao@163.com>
2019-01-31 11:03:13 +0800

    add column

--- article/views.py
+++ article/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from .models import ArticleColumn
+from .forms import ArticleColumnForm
 
 
 @login_required(login_url='/account/login/')
+@csrf_exempt
 def article_column(request):
-    columns = ArticleColumn.objects.filter(user=request.user)
-    return render(request, "article/column/article_column.html", {"columns": columns})
+    if request.method == "GET":
+        columns = ArticleColumn.objects.filter(user=request.user)
+        column_form = ArticleColumnForm()
+        return render(request, "article/column/article_column.html", {"columns": columns, 'column_form': column_form})
+
+    if request.method == "POST":
+        column_name = request.POST['column']
+        columns = ArticleColumn.objects.filter(user_id=request.user.id, column=column_name)
+        if columns:
+            return HttpResponse('2')
+        else:
+            ArticleColumn.objects.create(user=request.user, column=column_name)
+            return HttpResponse("1")
