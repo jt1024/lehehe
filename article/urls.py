4b9b7209aa61f586040f84eb6d461161c1862337
jt1024 <cctvjiatao@163.com>
2019-01-31 18:39:59 +0800

    article-post simply

--- article/urls.py
+++ article/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('article-column/', views.article_column, name="article_column"),
     path('rename-article-column/', views.rename_article_column, name="rename_article_column"),
     path('del-article-column/', views.del_article_column, name="del_article_column"),
+    path('article-post/', views.article_post, name="article_post"),
 ]
