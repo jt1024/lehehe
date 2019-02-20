d6f7d8ad867ddb110ff591bff490df7f2d0cd9b0
麦典威 <cctvjiatao@163.com>
2019-02-20 23:16:33 +0800

    use templatetags show total num of special author's articles

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -7,3 +7,8 @@ from article.models import ArticlePost
 @register.simple_tag
 def total_articles():
     return ArticlePost.objects.count()
+
+
+@register.simple_tag
+def author_total_articles(user):
+    return user.article.count()
