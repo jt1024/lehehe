35aa595dcc768310a336a6d86433e08100e7a5c8
麦典威 <cctvjiatao@163.com>
2019-02-27 22:48:06 +0800

    use templatetags show mostcomment articles

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -18,3 +18,11 @@ def author_total_articles(user):
 def latest_articles(n=5):
     latest_articles = ArticlePost.objects.order_by("-created")[:n]
     return {"latest_articles": latest_articles}
+
+
+from django.db.models import Count
+
+
+@register.simple_tag
+def most_commented_articles(n=3):
+    return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by("-total_comments")[:n]
