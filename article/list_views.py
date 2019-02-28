685c0060c90cc76843c94b8bcbba6b5c41def935
麦典威 <cctvjiatao@163.com>
2019-03-01 00:24:00 +0800

    recomend similar articles

--- article/list_views.py
+++ article/list_views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 import redis
 from django.conf import settings
@@ -61,7 +62,11 @@ def article_detail(request, id, slug):
     else:
         comment_form = CommentForm()
 
-    return render(request, "article/list/article_content.html", {"article": article, "total_views": total_views, "most_viewed": most_viewed, "comment_form": comment_form})
+    article_tags_ids = article.article_tag.values_list("id", flat=True)
+    similar_articles = ArticlePost.objects.filter(article_tag__in=article_tags_ids).exclude(id=article.id)
+    similar_articles = similar_articles.annotate(same_tags=Count("article_tag")).order_by('-same_tags', '-created')[:4]
+    return render(request, "article/list/article_content.html",
+                  {"article": article, "total_views": total_views, "most_viewed": most_viewed, "comment_form": comment_form, "similar_articles": similar_articles})
 
 
 @csrf_exempt
